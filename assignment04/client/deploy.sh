f8979b81c4cd8b05c21e442e731cbd96debc2669
Your Name <katedivushchak@gmail.com>
2021-12-30 20:34:54 +0200

    lab4

--- assignment04/client/deploy.sh
+++ assignment04/client/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building project...."
-PUBLIC_URL="/~kp9223/lab4" npm run build
+PUBLIC_URL="/~kp9307/lab4" npm run build
 
 echo "Rsync Project files to server:"
-rsync -rv -e "ssh -l kp9223 -p 6022" ./build/ sandbox.ee.kpi.ua:~/public_html/lab4
+rsync -rv -e "ssh -l kp9307 -p 6022" ./build/ sandbox.ee.kpi.ua:~/public_html/assignment4
